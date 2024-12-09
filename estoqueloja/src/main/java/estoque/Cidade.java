9d95521868998b30231e237f9768688865a9670f
Felipe Gomes <felipeferreiraag0@gmail.com>
2024-12-08 21:08:05 -0300

    Criando o metodo inserir na classe Cidade

--- estoqueloja/src/main/java/estoque/Cidade.java
+++ estoqueloja/src/main/java/estoque/Cidade.java
@@ -1,26 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package estoque;
-public class Cidade extends Entidade{
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author felipe Ferreira, Carlos Eduardo, Caua Alvarenga, Heitor Messias
+ * @since 08/12/2024 at 20:55
+ */
+public class Cidade extends Entidade {
     
-    private char[] uf = new char[2];
-    
-    public Cidade(int id, String nome, char[] uf){
+    private String nome;
+    private String uf;
+
+    public Cidade(int id,String nome, String uf ) {
         super(id, nome, "");
+        this.nome = nome;
         this.uf = uf;
     }
 
-    public char[] getUf() {
+   
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getUf() {
         return uf;
     }
-
-    public void setUf(char[] uf) {
-        this.uf = uf;
+    /**
+     * Metodo para inserier dados na tabela cidade
+     * @throws SQLException 
+     */
+    public void inserir() throws SQLException{
+        Connection conexao = new Conexao().getConexao();
+        String sql = "INSERT INTO cidade (cidade_uf, cidade_nome) Values (?, ?)";
+        
+        try {
+            PreparedStatement stmt;
+            stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, this.uf);
+            stmt.setString(2,this.nome);
+            stmt.setInt(3, Cidade.this.getId());
+            
+            stmt.execute();
+            stmt.close();
+            
+            conexao.close();
+            
+        } catch (SQLException e){
+            System.out.println("Erro ao inserir dados a tabela cidade! " + e.getMessage());
+            
+        }
+        
     }
     
-    @Override
-    public String toString(){
-        return "Dados: " + getId() + "\n"
-                + "Nome: " + getNome() + "\n"
-                + "UF: " + this.uf + "\n"
-                + "======================";
-    }
+    
 }
--- estoqueloja/src/main/java/estoque/Conexao.java
+++ estoqueloja/src/main/java/estoque/Conexao.java
@@ -4,19 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-    public class Conexao {
+public class Conexao {
 
-            public static final String SERVIDOR = "jdbc:mysql://localhost:3306/estoque_produtos";
-            public static final String USUARIO = "root";
-            public static final String SENHA = "123456789";
-    
-        public Connection getConexao() {
-            try {
-                return DriverManager.getConnection(SERVIDOR, USUARIO, SENHA);
-            } catch (Exception e) {
-                System.out.println("Erro ao conectar ao banco de dados");
-            }
-            return null;
+    public static final String SERVIDOR = "jdbc:mysql://localhost:3306/estoque_produtos";
+    public static final String USUARIO = "root";
+    public static final String SENHA = "123456789";
+
+    public Connection getConexao() {
+        try {
+            return DriverManager.getConnection(SERVIDOR, USUARIO, SENHA);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage() + "Erro ao fazer a conexao no banco de dados");
         }
-    }
+        return null;
 
+    }
+}
